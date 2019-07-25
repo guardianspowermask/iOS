59640c1d6181362ada9904f3ffbcd635c7924284
강수진 <rkdthd1234@naver.com>
2019-07-25 17:29:00 +0900

    파일 디렉토리 변경

---
+++ KoreanGuardians/KoreanGuardians/ViewController/Item/ItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemViewController.swift
+//  KoreanGuardians
+//
+//  Created by 강수진 on 25/07/2019.
+//  Copyright © 2019 강수진. All rights reserved.
+//
+
+import UIKit
+
+class ItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
