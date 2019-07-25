69bd3154fce58a283ef664ad84bb836ffc8529b0
sujin <rkdthd1234@naver.com>
2019-07-26 07:40:12 +0900

    mail delegate 작성

--- KoreanGuardians/KoreanGuardians/Common/MailUsable.swift
+++ KoreanGuardians/KoreanGuardians/Common/MailUsable.swift
@@ -29,6 +29,6 @@ extension MailUsable {
         }
     }
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-
+            controller.dismiss(animated: true, completion: nil)
     }
 }
