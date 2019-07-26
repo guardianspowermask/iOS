9ec8242a4d1b785cef710909e953517f0e43754c
강수진 <rkdthd1234@naver.com>
2019-07-27 02:11:30 +0900

    launchscreen gif 적용

--- KoreanGuardians/KoreanGuardians/ViewController/LaunchScreen/LaunchScreenViewController.swift
+++ KoreanGuardians/KoreanGuardians/ViewController/LaunchScreen/LaunchScreenViewController.swift
@@ -7,47 +7,31 @@
 //
 
 import UIKit
-import Lottie
+import SwiftGifOrigin
 
 class LaunchScreenViewController: UIViewController {
     private var mainStoryboard: UIStoryboard? {
         return UIStoryboard(name: "Main", bundle: nil)
     }
+    @IBOutlet var splashGifImg: UIImageView!
     override func viewDidAppear(_ animated: Bool) {
-        playLaunch()
+        playGIF()
     }
-    func playLaunch() {
-        guard let animationName = AnimationJson.allCases.randomElement()?.rawValue, let animationPath = Bundle.main.path(forResource: animationName, ofType: "json") else {
-            print("해당 json 파일을 찾을 수 없습니다")
-            self.goToMainVC()
-            return
-        }
-        let animation = Animation.filepath(animationPath)
-        let animationView = AnimationView(animation: animation)
-        animationView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-        animationView.center = self.view.center
-        animationView.contentMode = .scaleAspectFill
-        self.view.addSubview(animationView)
-        animationView.play(completion: { [weak self] (finished) in
+    func playGIF() {
+        let delayInSeconds = 3.2
+        let image = UIImage.gif(name: "pealluck")
+        splashGifImg.image = image
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delayInSeconds) { [weak self] in
             guard let `self` = self else {
                 return
             }
-            if finished {
-               self.goToMainVC()
-            }
-        })
+            self.goToMainVC()
+        }
     }
     func goToMainVC() {
         guard let mainVC = mainStoryboard?.instantiateViewController(withIdentifier: "mainNavi") else {
             return
         }
         self.present(mainVC, animated: true, completion: nil)
-    }
-}
-
-extension LaunchScreenViewController {
-    enum AnimationJson: String, CaseIterable {
-        case pumpedUp = "pumped_up"
-        case meowBoxSplash4 = "meow_box_splash_4"
     }
 }
