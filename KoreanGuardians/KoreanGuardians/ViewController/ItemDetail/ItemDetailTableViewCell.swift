7709928e6b91a539f0c7718789b0945889bdb001
강수진 <rkdthd1234@naver.com>
2019-08-16 01:14:33 +0900

    신고하기 셀에 추가

--- KoreanGuardians/KoreanGuardians/ViewController/ItemDetail/ItemDetailTableViewCell.swift
+++ KoreanGuardians/KoreanGuardians/ViewController/ItemDetail/ItemDetailTableViewCell.swift
@@ -12,10 +12,27 @@ class ItemDetailTableViewCell: UITableViewCell, NibLoadable {
     @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var contentLabel: UILabel!
     @IBOutlet private weak var dateLabel: UILabel!
-
-    func configure(data: String) {
+    @IBOutlet private weak var reporntButton: UIButton!
+    var reportItemBlock: ((_ row: Int) -> Void)?
+    private var row: Int?
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.setButtonAttr()
+    }
+    func setButtonAttr() {
+        self.reporntButton.addTarget(self, action: #selector(report), for: .touchUpInside)
+    }
+    @objc func report() {
+        reportItemBlock?(row ?? 0)
+    }
+    func setCallback(callback:@escaping (_ row: Int) -> Void) {
+        reportItemBlock = callback
+    }
+    func configure(data: String, row: Int) {
         nameLabel.text = data
         contentLabel.text = data
         dateLabel.text = data
+        self.row = row
     }
 }
+
